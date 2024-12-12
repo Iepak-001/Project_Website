13a8440f3b90db8ad0a7ab8cac67a3b89e359988
Deepak <deeepak2002@gmail.com>
2024-12-12 09:35:15 +0530

    video models and JWT Hooks

---
+++ src/models/video.models.js
@@ -0,0 +1,55 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+
+    }, 
+    {
+        timestamps: true
+    }
+)
+//Aggregation pipelines se bahut saara DB opperation mil jayega
+videoSchema.plugin(mongooseAggregatePaginate)
+
+//bcrypt used for pssword encryption
+
+//Jwt sending data ko encrypt krega
+
+//Hooks data sve hone se pahle fn chala dega
+
+//data save hone se pahle hook use krke encrypt kr denge
+
+export const Video = mongoose.model("Video", videoSchema)
